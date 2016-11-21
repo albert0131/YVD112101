c312e832a078d2ceac48e584106a3c0b0509e819
albert <alber@gmail.com>
2016-11-21 10:39:13 +0800

    利用boolean陣列chks來紀錄checkbox是否被打勾

--- app/src/main/java/com/example/user/yvd112101/MainActivity.java
+++ app/src/main/java/com/example/user/yvd112101/MainActivity.java
@@ -12,7 +12,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     ListView lv;
-    String fruits[] = {"蘋果", "西瓜", "楊桃", "鳳梨"};
+    String fruits[] = {"蘋果", "西瓜", "楊桃", "鳳梨", "蘋果1", "西瓜1", "楊桃1", "鳳梨1",
+            "蘋果2", "西瓜2", "楊桃2", "鳳梨2", "蘋果3", "西瓜3", "楊桃3", "鳳梨3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/user/yvd112101/MyAdapter.java
+++ app/src/main/java/com/example/user/yvd112101/MyAdapter.java
@@ -1,11 +1,14 @@
 package com.example.user.yvd112101;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +21,13 @@ public class MyAdapter extends BaseAdapter {
 
     Context context;
     String[] data;
+    boolean[] chks;             // for 紀錄checkbox是否被打勾
+
     public MyAdapter(Context context, String[] d) {
 
         this.context = context;
         this.data = d;
+        chks = new boolean[d.length];
     }
     @Override
     public int getCount() {
@@ -40,6 +46,8 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
+
+        Log.d("LV", "getView, position:" + position + ", content:" + data[position]);
         LayoutInflater inflater = LayoutInflater.from(context);
         View v = inflater.inflate(R.layout.myitem, null);
         TextView tv = (TextView) v.findViewById(R.id.textView);
@@ -54,6 +62,16 @@ public class MyAdapter extends BaseAdapter {
             }
         });
 
+        // 利用boolean陣列chks來紀錄checkbox是否被打勾, 避免捲頁時資料被改寫
+        CheckBox chkbox = (CheckBox) v.findViewById(R.id.checkBox);
+        chkbox.setChecked(chks[position]);
+
+        chkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                chks[position] = isChecked;
+            }
+        });
         return v;
     }
 }
